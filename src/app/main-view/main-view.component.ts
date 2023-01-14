842b293a4ff2e3e7d04b1950f6a5020bb2ba1aee
MatChumski <mateo0704@hotmail.com>
2023-01-14 17:25:27 -0500

    First commit

---
+++ src/app/main-view/main-view.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-view',
+  templateUrl: './main-view.component.html',
+  styleUrls: ['./main-view.component.css']
+})
+
+export class MainViewComponent {
+
+  logo:string = "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ab/Android_O_Preview_Logo.png/1024px-Android_O_Preview_Logo.png";
+
+}
