3cf7dbdb06092d075873217f7cac36ba8a474481
MatChumski <mateo0704@hotmail.com>
2023-01-14 22:48:37 -0500

    Main view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,28 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainViewComponent } from './main-view/main-view.component';
 import { MainHeaderComponent } from './main-header/main-header.component';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+
+const appRoutes:Routes=[
+  {path:'', component:MainViewComponent},
+  {path:'login', component:LoginComponent},
+  {path:'signup', component:SignupComponent}
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     MainViewComponent,
-    MainHeaderComponent
+    MainHeaderComponent,
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
